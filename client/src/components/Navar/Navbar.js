c32b16efcc55e3c576d6c81e3e04b9f70876cf1a
XinzeWang <wangxinze_1995@163.com>
2021-03-21 16:54:48 -0400

    update title

--- client/src/components/Navar/Navbar.js
+++ client/src/components/Navar/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer} >
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Pretty Pretty Nice Memories ✍️</Typography>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Pretty Pretty Nice</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />         
             </div>    
             <Toolbar className={classes.toolbar}>
