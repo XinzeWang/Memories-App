927192c61522ae309f52fda28255c1d5f525b59b
XinzeWang <wangxinze_1995@163.com>
2021-03-21 16:38:01 -0400

    update baseurl for backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import { pink } from '@material-ui/core/colors';
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'https://momories-project-666.herokuapp.com/'})
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
